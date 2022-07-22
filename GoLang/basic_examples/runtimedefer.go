1d850fef8627be4aa26a6f88cb3e106d7aecb534
Ulaş Erdoğan <uerdogan2001@hotmail.com>
2022-07-23 01:55:38 +0300

    exc time by defer

---
+++ GoLang/basic_examples/runtimedefer.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func timer(name string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n", name, time.Since(start))
+	}
+}
+
+func main() {
+	defer timer("main")()
+	time.Sleep(time.Second * 2)
+}
